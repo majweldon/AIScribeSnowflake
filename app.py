d4b069bac219ddaafed299f3b574d2d37cc30d5b
Mike <majweldon@users.noreply.huggingface.co>
2024-01-19 23:35:58 +0000

    Update app.py
    Changed 2nd LLM to  LLaMA2

--- app.py
+++ app.py
@@ -5,27 +5,28 @@ from numpy import True_
 import gradio as gr
 import soundfile as sf
 from pydub import AudioSegment
-
 from openai import OpenAI
-
-from mistralai.client import MistralClient
-from mistralai.models.chat_completion import ChatMessage
-
+#from mistralai.client import MistralClient
+#from mistralai.models.chat_completion import ChatMessage
+import replicate
 
 # Load API keys from an environment variables
 OPENAI_SECRET_KEY = os.environ.get("OPENAI_SECRET_KEY")
-api_key = os.environ["MISTRAL_API_KEY"]
-mistral_model = "mistral-small"
+REPLICATE_API_KEY = os.environ.get("REPLICATE_API_TOKEN")
+
+#api_key = os.environ["MISTRAL_API_KEY"]
+#mistral_model = "mistral-tiny"
 
 client = OpenAI(api_key = OPENAI_SECRET_KEY)
-mistral_client = MistralClient(api_key=api_key)
+#mistral_client = MistralClient(api_key=api_key)
 
 
 note_transcript = ""
 mistral_note_transcript = ""
+llama_note_transcript = ""
 
 def transcribe(audio, history_type):
-  global note_transcript    
+  global note_transcript
   history_type_map = {
       "History": "Weldon_History_Format.txt",
       "Physical": "Weldon_PE_Note_Format.txt",
@@ -39,28 +40,27 @@ def transcribe(audio, history_type):
       "Psych": "Weldon_Psych_Format.txt",
       "SBAR": "SBAR.txt"
    }
-    
+
   file_name = history_type_map.get(history_type, "Weldon_Full_Visit_Format.txt")
   with open(f"Format_Library/{file_name}", "r") as f:
       role = f.read()
-    
+
   messages = [{"role": "system", "content": role}]
-  mistral_messages = [ChatMessage(role="system", content=role)]
-
-
+  #mistral_messages = [ChatMessage(role="system", content=role)]
+  
   ################# Create Dialogue Transcript from Audio Recording and Append(via Whisper)
-    
+
   audio_data, samplerate = sf.read(audio) # read audio from filepath
   #samplerate, audio_data = audio  # read audio from numpy array
 
- 
+
   ########## Cast as float 32, normalize
   #audio_data = audio_data.astype("float32")
   #audio_data = (audio_data * 32767).astype("int16")
   #audio_data = audio_data.mean(axis=1)
 
   #sf.write("Audio_Files/test.mp3", audio_data, samplerate)
-    
+
   ###################Code to convert .wav to .mp3
   sf.write("Audio_Files/test.wav", audio_data, samplerate, subtype='PCM_16')
   sound = AudioSegment.from_wav("Audio_Files/test.wav")
@@ -69,8 +69,8 @@ def transcribe(audio, history_type):
 
   ################  Send file to Whisper for Transcription
   audio_file = open("Audio_Files/test.mp3", "rb")
-  
-    
+
+
   max_attempts = 3
   attempt = 0
   while attempt < max_attempts:
@@ -82,15 +82,15 @@ def transcribe(audio, history_type):
           attempt += 1
           time.sleep(3) # wait for x seconds before retrying
   else:
-      print("Failed to transcribe audio after multiple attempts")  
-    
+      print("Failed to transcribe audio after multiple attempts")
+
   print(audio_transcript.text)
   messages.append({"role": "user", "content": audio_transcript.text})
 
-  mistral_messages.append(ChatMessage(role="user", content=audio_transcript.text))
+  #mistral_messages.append(ChatMessage(role="user", content=audio_transcript.text))
 
 
-  
+
   ### Word and MB Count
   file_size = os.path.getsize("Audio_Files/test.mp3")
   mp3_megabytes = file_size / (1024 * 1024)
@@ -101,24 +101,40 @@ def transcribe(audio, history_type):
 
 
   #Ask OpenAI to create note transcript
-  ## 1.1.1  
+  ## 1.1.1
   response = client.chat.completions.create(model="gpt-4-1106-preview", temperature=0, messages=messages)
   note_transcript = response.choices[0].message.content
 
   print("\n\n" + note_transcript + "\n\n")
 
 
-
-
   #Ask Mistral to create note transcript
   ## 0.0.1
-  mistral_response = mistral_client.chat(model=mistral_model, temperature=0, messages=mistral_messages)
-  mistral_note_transcript = mistral_response.choices[0].message.content
+  #mistral_response = mistral_client.chat(model=mistral_model, temperature=0, messages=mistral_messages)
+  #mistral_note_transcript = mistral_response.choices[0].message.content
+  #print (mistral_note_transcript)
 
-  print (mistral_note_transcript)
+  #Ask LLaMA2 (via Replicate) to create note transcript
+  llama_output = replicate.run(
+    #"meta/llama-2-7b-chat:f1d50bb24186c52daae319ca8366e53debdaa9e0ae7ff976e918df752732ccc4",
+    "meta/llama-2-70b-chat:2d19859030ff705a87c746f7e96eea03aefb71f166725aee39692f1476566d48",
+    input={
+        "debug": False,
+        "top_p": 1,
+        "prompt": audio_transcript.text,
+        "temperature": 0.01,
+        "system_prompt": role,
+        "max_new_tokens": 500,
+        "min_new_tokens": -1
+    }
+  )
+  llama_note_transcript = ""
+  for item in llama_output:
+    llama_note_transcript += item
+  print(llama_note_transcript)
 
 
-  return [note_transcript, mistral_note_transcript, mp3_megabytes]
+  return [note_transcript, llama_note_transcript, mp3_megabytes]
 
 
 
@@ -130,10 +146,10 @@ my_inputs = [
     gr.Radio(["History","H+P","Impression/Plan","Full Visit","Handover","Psych","EMS","SBAR","Meds Only"], show_label=False),
 ]
 
-ui = gr.Interface(fn=transcribe, 
-                  inputs=my_inputs, 
+ui = gr.Interface(fn=transcribe,
+                  inputs=my_inputs,
                   outputs=[gr.Textbox(label="OpenAI Note", show_copy_button=True),
-                           gr.Textbox(label="MistralAI Note", show_copy_button=True),
+                           gr.Textbox(label="LLaMa2 AI Note", show_copy_button=True),
                            gr.Number(label=".mp3 MB")])
 
 
