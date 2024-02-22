d2d9afff17f2aba5bf97e831ed107488e9eb962d
lonergan123 <lonergan123@hotmail.com>
2024-02-22 23:39:42 +0000

    Add use_test_audio option to transcribe function

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_logger(logger_name):
 
 logger = get_logger('service-to-service')
 
-def transcribe(audio, model_name, history_type, request: gr.Request):
+def transcribe(audio, use_test_audio, model_name, history_type, request: gr.Request):
   global note_transcript
   history_type_map = {
       "History": "Weldon_History_Format.txt",
@@ -44,13 +44,18 @@ def transcribe(audio, model_name, history_type, request: gr.Request):
       role = f.read()
   
   ################# Create Dialogue Transcript from Audio Recording and Append(via Whisper)
-
-  audio_data, samplerate = sf.read(audio) # read audio from filepath
+  
+  if not use_test_audio:
+    audio_data, samplerate = sf.read(audio) # read audio from filepath
     
-  ###################Code to convert .wav to .mp3
-  sf.write("audio_files/test.wav", audio_data, samplerate, subtype='PCM_16')
-  sound = AudioSegment.from_wav("audio_files/test.wav")
-  sound.export("audio_files/test.mp3", format="mp3")
+    ###################Code to convert .wav to .mp3
+    sf.write("audio_files/test.wav", audio_data, samplerate, subtype='PCM_16')
+    sound = AudioSegment.from_wav("audio_files/test.wav")
+    sound.export("audio_files/test.mp3", format="mp3")
+  else:
+    # copy /Test_Audio_Files/Test_Elbow.mp3 to /audio_files/test.mp3
+    subprocess.run(["cp", "Test_Audio_Files/Test_Elbow.mp3", "audio_files/test.mp3"])
+    logger.info("Using test audio file")
 
   ###################Call Whister Service in SCS
   service_url = os.getenv('WHISPER_API', 'http://whisper-app.kl-test-jenkins.db-team-jenkins.snowflakecomputing.internal:9000/transcripe_stage_audio') 
@@ -62,39 +67,41 @@ def transcribe(audio, model_name, history_type, request: gr.Request):
   whisper_response = whisper_response.text
 
   if whisper_response is None:
-    logger.error('Received empty response from service ' + service_url)
+    logger.error('Received empty response from service' + service_url)
 
   # Setup prompt message using mistral format
-  messages=[
-      {"role": "user", "content": role},
-      {"role": "assistant", "content": whisper_response},
-  ]
+  
+
+  prompt_txt = "<sr>[INST]" + role + whisper_response + "[/INST]" 
+  logger.info(f'Prompt Text: {prompt_txt}')
 
   ###################Call LLM Service in SCS
   openai_api_base = os.getenv('OPENAI_API', 'http://kl-vllm-mistral.kl-test-jenkins.db-team-jenkins.snowflakecomputing.internal:8000/v1') 
   logger.info(f'Calling {openai_api_base}')
   api_headers = {'Content-Type': 'application/json'}
-
+  
   openai_api_key = "EMPTY"
   client = OpenAI(
     api_key=openai_api_key,
     base_url=openai_api_base,
     default_headers=api_headers
 )
-# %%
   try:
-    response = client.completions.create(model="/models/mistral/",
-                      prompt=messages,
-                      max_tokens=500,
-                      n=1,
-                      stop=None,
-                      temperature=0)
-    note_transcript = response.choices[0].message.content
+    response = client.completions.create(
+      model="/models/mistral/",
+      prompt=prompt_txt,
+      max_tokens=500,
+      stream=False,
+      temperature=0
+      )
+
+    note_transcript = response.choices[0].text.strip() 
+    logger.info(f'Note Transcript: {note_transcript}')
   except Exception as e:
     logger.error(f"Error occurred during LLM completion: {str(e)}")
     note_transcript = "Error occurred during LLM completion"
 
-    ### Word and MB Count
+  ### Word and MB Count
   file_size = os.path.getsize("audio_files/test.mp3")
   mp3_megabytes = file_size / (1024 * 1024)
   mp3_megabytes = round(mp3_megabytes, 2)
@@ -102,17 +109,14 @@ def transcribe(audio, model_name, history_type, request: gr.Request):
   headers = request.headers
   sf_user = headers["Sf-Context-Current-User"]
   
-  transcription_text = 'LLM not yet available'
-  # audio_transcript_words = audio_transcript.text.split() # Use when using mic input
-  # num_words = len(audio_transcript_words)
   return [whisper_response, note_transcript, mp3_megabytes, sf_user]
 
 ###################### Define Gradio Interface ######################
 
 my_inputs = [
     gr.Audio(source="microphone", type="filepath"), #Gradio 3.48.0
+    gr.Checkbox(label="Use Test_Elbow.mp3 (overrides mic input)"),
     # gr.File(file_types=['.mp3', '.wav'], label="Upload Audio File (overrides microphone input - for TESTING)"),
-    # add an drop-down that allows selection of the model, and default this to mistralai/Mistral-7B-Instruct-v0.2
     gr.Dropdown(['mistralai/Mistral-7B-Instruct-v0.2'], value= 'mistralai/Mistral-7B-Instruct-v0.2', label="Summarization LLM"),
     gr.Radio(["History","H+P","Impression/Plan","Full Visit","Handover","Psych","EMS","SBAR","Meds Only"], show_label=False)
     #gr.Audio(sources=["microphone"],type="numpy"), #Gradio 4.7.1
